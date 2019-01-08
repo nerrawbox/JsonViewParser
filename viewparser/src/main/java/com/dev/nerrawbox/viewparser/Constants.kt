1564ce86545504c39c0b181b06216102566914be
Warren Cedro <nerrawbox@gmail.com>
2019-01-08 11:54:46 +0800

    Constants class added. Constants class contains the key for values inside the json file

---
+++ viewparser/src/main/java/com/dev/nerrawbox/viewparser/Constants.kt
@@ -0,0 +1,13 @@
+package com.dev.nerrawbox.viewparser
+
+class Constants {
+    val LAYOUT_INFO = "layout_info"
+    val LAYOUT_ID = "layout_id"
+    val LAYOUT_ORIENTATION = "layout_orientation"
+    val LAYOUT_PARAMS = "layout_params"
+    val LAYOUT_PARAMS_WIDTH = "width"
+    val LAYOUT_PARAMS_HEIGHT = "height"
+
+    val VIEW_WIDGETS = "view_widgets"
+
+}
