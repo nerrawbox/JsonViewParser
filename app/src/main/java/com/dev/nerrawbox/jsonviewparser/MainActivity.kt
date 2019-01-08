6450abb36e05e310da82f16f3e816ed5f4f24623
Warren Cedro <nerrawbox@gmail.com>
2019-01-08 15:28:36 +0800

    Added ImageView for test

--- app/src/main/java/com/dev/nerrawbox/jsonviewparser/MainActivity.kt
+++ app/src/main/java/com/dev/nerrawbox/jsonviewparser/MainActivity.kt
@@ -3,12 +3,8 @@ package com.dev.nerrawbox.jsonviewparser
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
+import android.widget.*
 import com.dev.nerrawbox.viewparser.ViewParser
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +17,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val parentLayout: LinearLayout = findViewById(R.id.myLinearLayout)
+        val viewParser = ViewParser(this, parentLayout, "screen.json", "MainActivity")
 
+        myWidgets = viewParser.initViews()
+        myTextView = myWidgets!![0] as TextView
+        myEditText = myWidgets!![1] as EditText
+        myBtn = myWidgets!![2] as Button
+
+        myTextView?.text = "Hello World"
+        myEditText?.hint = "Username"
+
+        myBtn?.text = "Submit"
+        myBtn?.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Clicked", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
